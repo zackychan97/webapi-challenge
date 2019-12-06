21c0b387ec3227977e19250ebb844d6e6e5b6580
Zack Bumpous <upwork.ztb@gmail.com>
2019-12-06 10:52:40 -0600

    Adds express into my challenge-routes file as well as sets up my router and imports data, also adds server.use for api/projects

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const challengeRouter = require("./projects-route");
 const server = express();
 
 server.use(express.json());
 
 server.get("/", (req, res) => {
     res.status(200).json({ message: "server is up and running" })
-})
+});
+
+server.use("/api/projects", projectRouter);
 
 module.exports = server;
