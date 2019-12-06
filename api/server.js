09ff7c5068d63cfcef7c7c0fad975e9ce84378c3
Zack Bumpous <upwork.ztb@gmail.com>
2019-12-06 12:11:12 -0600

    Adds get / and get /:id for actions and tests out, have PUT for actions by /:id written out and about to test...

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
   
 const express = require("express");
 const projectRouter = require("./challenge-routes");
+const actionsRouter = require("./action-routes");
 const server = express();
 
 server.use(express.json());
@@ -10,5 +11,6 @@ server.get("/", (req, res) => {
 });
 
 server.use("/api/projects", projectRouter);
+server.use("/api/actions", actionsRouter)
 
 module.exports = server;
